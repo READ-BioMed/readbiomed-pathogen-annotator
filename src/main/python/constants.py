3da7b25054646f3f64271a1c90068625c6989673
Antonio Jimeno Yepes <antonio.jimeno@gmail.com>
2021-08-05 11:46:21 +1000

    longformer

--- src/main/python/constants.py
+++ src/main/python/constants.py
@@ -1,4 +1,7 @@
 model_name = "bert-base-uncased"
+model_name = "allenai/longformer-base-4096"
+#model_name = "google/bigbird-pegasus-large-pubmed"
 device = "cpu"
 max_len=250
+max_len=1250
 model_file = "./model-non-relevant-document-bert.torch"
